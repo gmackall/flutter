f4a9d0a3b3f099898a24351624b18b56586eabcb
Gray Mackall <mackall@google.com>
2023-09-18 10:21:52 -0700

    unpin

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -35,8 +35,6 @@ const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   // Keep pinned to latest until 1.0.0.
   'material_color_utilities': '0.5.0',
   // https://github.com/flutter/flutter/issues/111304
-  'url_launcher_android': '6.0.17',
-  // https://github.com/flutter/flutter/issues/115660
   'archive': '3.3.2',
 };
 
