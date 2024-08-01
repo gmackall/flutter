d2c821f06f0ac59398830e8f2779d07bcbc37dac
Gray Mackall <mackall@google.com>
2024-08-01 10:17:25 -0700

    initial wip

---
+++ packages/flutter_tools/flutter_gradle_plugin/src/main/java/com/flutter/gradle/MyJavaPlugin.java
@@ -0,0 +1,13 @@
+package com.flutter.gradle;
+
+import org.gradle.api.Project;
+import org.gradle.api.Plugin;
+
+public class MyJavaPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+        //target.task("javaTask");
+        System.out.println("HI GRAY");
+    }
+}
